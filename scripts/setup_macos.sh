ee02f7c306566a1a8a9f9976ac68be853be5cbe3
Ryan Peden <ryan.peden@gmail.com>
2025-11-29 16:08:30 -0600

    Expand macOS defaults, add Dock automation

--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -2,23 +2,77 @@
 
 set -e
 
+# ==============================================================================
+# Defaults
+# ==============================================================================
+
 # Keyboard
 defaults write -g ApplePressAndHoldEnabled -bool false
-defaults write -g KeyRepeat -int 1
 defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
+defaults write -g NSAutomaticCapitalizationEnabled -bool false
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 
 # Trackpad
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 
 # Finder
 defaults write -g AppleShowAllExtensions -bool true
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/work/"
-defaults write com.apple.finder ShowPathbar  -bool true
+defaults write com.apple.finder ShowPathbar -bool true
 
 # Dock
+defaults write com.apple.dock launchanim -bool false
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock tilesize -int 35
+
+# Menu Bar
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+# Dialogs
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
+defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
+defaults write -g PMPrintingExpandedStateForPrint -bool true
+defaults write -g PMPrintingExpandedStateForPrint2 -bool true
+
+# ==============================================================================
+# Dock
+# ==============================================================================
+
+DOCK_APPS=(
+    "/Applications/Google Chrome.app"
+    "/System/Applications/Notes.app"
+    "/Applications/Spotify.app"
+    "/System/Applications/Podcasts.app"
+    "/System/Applications/Photos.app"
+    "/Applications/Visual Studio Code.app"
+    "/System/Applications/Utilities/Terminal.app"
+    "/Applications/zoom.us.app"
+    "/Applications/Citrix Workspace.app"
+    "/System/Applications/System Settings.app"
+)
+
+DOCK_FOLDERS=(
+    "$HOME/work"
+    "$HOME/Downloads"
+)
+
+dockutil --remove all --no-restart
+
+for app in "${DOCK_APPS[@]}"; do
+    [[ -d "$app" ]] && dockutil --add "$app" --no-restart
+done
+
+for folder in "${DOCK_FOLDERS[@]}"; do
+    [[ -d "$folder" ]] && dockutil --add "$folder" --view list --display folder --no-restart
+done
+
+# ==============================================================================
+# Restart
+# ==============================================================================
 
 killall cfprefsd
 killall Dock
